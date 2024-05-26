4d2fe727b9e9dc2f0e5e055967d304e16f8e072a
lain-alice <freya6829@gmail.com>
2024-05-27 01:21:08 +0900

    feat: 동적 함수 및 쿼리 키

--- 24_리액트_쿼리/src/components/Events/FindEventSection.jsx
+++ 24_리액트_쿼리/src/components/Events/FindEventSection.jsx
@@ -1,10 +1,27 @@
-import { useRef } from 'react';
+import { useState, useRef } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { fetchEvents } from "../../util/http";
 
 export default function FindEventSection() {
   const searchElement = useRef();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useQuery({
+    queryKey: ["events", { search: searchElement.current.value }],
+    // 모든 이벤트가 아닌 검색어와 일치하는 이벤트만 가져움
+    // 아래의 ref를 여기 또 쓰는 건 적절하지 않음, ref는 함수 다시 실행되도록 할 수 없음
+    queryFn: () => fetchEvents(searchElement.current.value),
+  });
+  // input에 입력된 검색어가 fetchEvents에 전달돼야 함
+  // 검색어가 없으면 쿼리 전송되지 않도록 해야 함
+
+  // queryKey를 events로 하면 NewEventsSection의 쿼리 결과를 캐시 처리해서 다른 컴포넌트에서도 재사용
+  // 올바른 방법이 아님, 모든 결과가 아니라 현재 검색중인 일부 결과만 필요함
+  // 이 쿼리는 독립적으로 실행되고 NewEventsSection의 쿼리 결과는 이용하지 않아야 함
 
   function handleSubmit(event) {
     event.preventDefault();
+    setSearchTerm(searchElement.current.value);
   }
 
   return (
--- 24_리액트_쿼리/src/util/http.js
+++ 24_리액트_쿼리/src/util/http.js
@@ -1,5 +1,19 @@
-export async function fetchEvents() {
-  const response = await fetch("http://localhost:3000/events");
+export async function fetchEvents(searchTerm) {
+  let url = "http://localhost:3000/events";
+  // url을 동적으로 구성
+
+  if (searchTerm) {
+    // searchTerm이 빈 문자 아니고 값 있으면
+
+    url += "?serach=" + searchTerm;
+  }
+
+  // 검색결과 없어도 함수 호출 가능, 검색어 있으면 백엔드에 보내줌
+  // 백엔드에서는 검색 결과와 맞는 결과 제공
+
+  const response = await fetch(url);
+  //  FindEventSection 요청 발생 시 쿼리 매개변수 추가
+  // 그냥 하드코딩한 url을 동적인 url로 대체
 
   if (!response.ok) {
     const error = new Error("An error occurred while fetching the events");
