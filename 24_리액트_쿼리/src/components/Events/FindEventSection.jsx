4301d744b2da1f0fb3e546a29f30956aef6ee690
lain-alice <freya6829@gmail.com>
2024-06-05 00:31:32 +0900

    feat: 검색 기능

--- 24_리액트_쿼리/src/components/Events/FindEventSection.jsx
+++ 24_리액트_쿼리/src/components/Events/FindEventSection.jsx
@@ -1,27 +1,66 @@
 import { useState, useRef } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { fetchEvents } from "../../util/http";
+import LoadingIndicator from "../UI/LoadingIndicator";
+import ErrorBlock from "../UI/ErrorBlock";
+import EventItem from "./EventItem";
 
 export default function FindEventSection() {
   const searchElement = useRef();
   const [searchTerm, setSearchTerm] = useState("");
 
-  useQuery({
-    queryKey: ["events", { search: searchElement.current.value }],
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", { search: searchTerm }],
     // 모든 이벤트가 아닌 검색어와 일치하는 이벤트만 가져움
-    // 아래의 ref를 여기 또 쓰는 건 적절하지 않음, ref는 함수 다시 실행되도록 할 수 없음
-    queryFn: () => fetchEvents(searchElement.current.value),
+    // 쿼리키가 검색결과 정보 포함, 이 정보는 동적으로 변해야 함, 검색어만 해당하니까
+
+    // 아래의 ref를 여기 또 쓰는 건 적절하지 않음, ref는 상태와 달리 함수 다시 실행되도록 할 수 없음
+    // 입력값이 변해도 이 쿼리는 업데이트, 재전송 x
+    // 사용자가 다른 검색어 입력하면 새 데이터 가져오도록 하려면?
+    queryFn: ({ signal }) => fetchEvents({ signal, searchTerm }),
+    // 리액트 쿼리에서 실제로 리액트 쿼리에서 호출될 함수
+    // searchTerm으로 fetchEvents, queryKey 동적 업데이트
+    // fetchEvents 함수에 searchTerm이라 이름지은 프로퍼티가 담긴 객체 전달
   });
   // input에 입력된 검색어가 fetchEvents에 전달돼야 함
   // 검색어가 없으면 쿼리 전송되지 않도록 해야 함
 
-  // queryKey를 events로 하면 NewEventsSection의 쿼리 결과를 캐시 처리해서 다른 컴포넌트에서도 재사용
+  // queryKey를 events로 하면 리액트 쿼리는 NewEventsSection의 쿼리 결과를 캐시 처리해서 다른 컴포넌트에서도 재사용
   // 올바른 방법이 아님, 모든 결과가 아니라 현재 검색중인 일부 결과만 필요함
   // 이 쿼리는 독립적으로 실행되고 NewEventsSection의 쿼리 결과는 이용하지 않아야 함
 
   function handleSubmit(event) {
     event.preventDefault();
     setSearchTerm(searchElement.current.value);
+    // 추천 이벤트 값은 폼까지 제출한 사람만
+  }
+
+  let content = <p>Please enter a search term and to find events.</p>;
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title="An Error occurred"
+        message={error.info?.message || "Failed to fetch events."}
+      />
+    );
+    // info가 undefined 아닐 때만 message 액세스
+  }
+
+  if (data) {
+    content = (
+      <ul className="events-list">
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
   }
 
   return (
@@ -37,7 +76,7 @@ export default function FindEventSection() {
           <button>Search</button>
         </form>
       </header>
-      <p>Please enter a search term and to find events.</p>
+      {content}
     </section>
   );
 }
--- 24_리액트_쿼리/src/util/http.js
+++ 24_리액트_쿼리/src/util/http.js
@@ -1,4 +1,11 @@
-export async function fetchEvents(searchTerm) {
+export async function fetchEvents({ signal, searchTerm }) {
+  console.log(searchTerm);
+
+  // console.log(searchTerm); 하면 나오는 객체... 쿼리키 정보와 요청 취소 신호 전달하는 객체
+  // 요청 완료되기 전에 사용자가 나가거나 하면 리액트쿼리가 자동으로 취소해줌
+  // searchTerm을 그냥 받으면 객체 채로 url에 들어감
+  // 중괄호 치고 구조분해 할당으로 필요한 키의 밸류만 받아야
+
   let url = "http://localhost:3000/events";
   // url을 동적으로 구성
 
@@ -11,9 +18,10 @@ export async function fetchEvents(searchTerm) {
   // 검색결과 없어도 함수 호출 가능, 검색어 있으면 백엔드에 보내줌
   // 백엔드에서는 검색 결과와 맞는 결과 제공
 
-  const response = await fetch(url);
+  const response = await fetch(url, { signal: signal });
   //  FindEventSection 요청 발생 시 쿼리 매개변수 추가
   // 그냥 하드코딩한 url을 동적인 url로 대체
+  // signal:signal을 전달해야 내부에서 취소 신호 받으면 요청 취소 가능
 
   if (!response.ok) {
     const error = new Error("An error occurred while fetching the events");
