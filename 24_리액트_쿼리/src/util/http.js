79203b67309bb8e27bf7c6c9b39fae43f4bf103a
lain-alice <freya6829@gmail.com>
2024-08-12 18:00:43 +0900

    feat: 이벤트 내용 수정

--- 24_리액트_쿼리/src/util/http.js
+++ 24_리액트_쿼리/src/util/http.js
@@ -102,8 +102,21 @@ export async function deleteEvent({ id }) {
   return response.json();
 }
 
-// 검색결과 없어도 함수 호출 가능, 검색어 있으면 백엔드에 보내줌
-// 백엔드에서는 검색 결과와 맞는 결과 제공
-//  FindEventSection 요청 발생 시 쿼리 매개변수 추가
-// 그냥 하드코딩한 url을 동적인 url로 대체
-// signal:signal을 전달해야 내부에서 취소 신호 받으면 요청 취소 가능
+export async function updateEvent({ id, event }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({ event }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while updating the event");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  return response.json();
+}
